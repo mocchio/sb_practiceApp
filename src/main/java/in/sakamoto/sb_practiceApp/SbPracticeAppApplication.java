8391518dafe09180619adff35928c2667387b8ec
mocchio <skmt.rome@gmail.com>
2023-05-23 12:06:21 +0900

    Initial commit

---
+++ src/main/java/in/sakamoto/sb_practiceApp/SbPracticeAppApplication.java
@@ -0,0 +1,13 @@
+package in.sakamoto.sb_practiceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPracticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbPracticeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/sakamoto/sb_practiceApp/SbPracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sakamoto.sb_practiceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbPracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
