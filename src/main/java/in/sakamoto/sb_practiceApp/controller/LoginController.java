f57d307dd1b6793d145c95a2e8523f668a2733cd
mocchio <skmt.rome@gmail.com>
2023-05-24 14:05:20 +0900

    ログイン画面のコントローラー作成

--- src/main/java/in/sakamoto/sb_practiceApp/controller/LoginController.java
+++ src/main/java/in/sakamoto/sb_practiceApp/controller/LoginController.java
@@ -1,4 +1,13 @@
 package in.sakamoto.sb_practiceApp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+    // ログイン画面を表示
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login/login";
+    }
 }
