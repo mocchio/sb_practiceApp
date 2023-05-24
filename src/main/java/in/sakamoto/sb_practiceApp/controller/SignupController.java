921d73fcd347e2b758b26831dbea9111ca6fa392
mocchio <skmt.rome@gmail.com>
2023-05-24 14:15:55 +0900

    ユーザー登録処理のコントローラー作成

--- src/main/java/in/sakamoto/sb_practiceApp/controller/SignupController.java
+++ src/main/java/in/sakamoto/sb_practiceApp/controller/SignupController.java
@@ -1,4 +1,35 @@
 package in.sakamoto.sb_practiceApp.controller;
 
+import in.sakamoto.sb_practiceApp.application.service.UserApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/user")
 public class SignupController {
+    @Autowired
+    private UserApplicationService userApplicationService;
+
+    /* ユーザー登録画面の表示 */
+    @GetMapping("/signup")
+    public String getSignup(Model model) {
+        // 性別を取得
+        Map<String, Integer> genderMap = userApplicationService.getGenderMap();
+        model.addAttribute("genderMap", genderMap);
+        // ユーザー登録画面に遷移
+        return "user/signup";
+    }
+
+    /* ユーザー登録処理 */
+    @PostMapping("/signup")
+    public String postSingup() {
+        // ログイン画面にリダイレクト
+        return "redirect:/login";
+    }
 }
