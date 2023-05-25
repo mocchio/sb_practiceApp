2f659a2ec654304426fe3638900261c0b3417609
mocchio <skmt.rome@gmail.com>
2023-05-25 12:29:37 +0900

    フォームクラスの作成と@Slf4j@ModelAttributeアノテーションの使用

--- src/main/java/in/sakamoto/sb_practiceApp/controller/SignupController.java
+++ src/main/java/in/sakamoto/sb_practiceApp/controller/SignupController.java
@@ -1,10 +1,13 @@
 package in.sakamoto.sb_practiceApp.controller;
 
 import in.sakamoto.sb_practiceApp.application.service.UserApplicationService;
+import in.sakamoto.sb_practiceApp.form.SignupForm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,13 +16,14 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
     @Autowired
     private UserApplicationService userApplicationService;
 
     /* ユーザー登録画面の表示 */
     @GetMapping("/signup")
-    public String getSignup(Model model, Locale locale) {
+    public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
         // 性別を取得
         Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
         model.addAttribute("genderMap", genderMap);
@@ -29,7 +33,8 @@ public class SignupController {
 
     /* ユーザー登録処理 */
     @PostMapping("/signup")
-    public String postSingup() {
+    public String postSingup(@ModelAttribute SignupForm form) {
+        log.info(form.toString());
         // ログイン画面にリダイレクト
         return "redirect:/login";
     }
