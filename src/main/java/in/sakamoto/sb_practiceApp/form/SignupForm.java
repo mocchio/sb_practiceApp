50d2abb04009bfc34041b5d80964f67f94b4509c
mocchio <skmt.rome@gmail.com>
2023-05-28 10:16:52 +0900

    フォームのアノテーション記述

--- src/main/java/in/sakamoto/sb_practiceApp/form/SignupForm.java
+++ src/main/java/in/sakamoto/sb_practiceApp/form/SignupForm.java
@@ -1,19 +1,36 @@
 package in.sakamoto.sb_practiceApp.form;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
 public class SignupForm {
+
+    @NonNull
+    @Email
     private String userId;
+
+    @NotBlank
+    @Length(min = 4, max = 100)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String password;
+
+    @NotBlank
     private String userName;
 
     @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @NotNull
     private Date birthday;
 
+    @Min(20)
+    @Max(100)
     private Integer age;
+
+    @NotNull
     private Integer gender;
 }
