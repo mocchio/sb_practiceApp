d4afd8b3387aa466f218842613f2d159608e983a
mocchio <skmt.rome@gmail.com>
2023-05-30 11:18:44 +0900

    ユーザー一覧画面のコントローラー設定

--- src/main/java/in/sakamoto/sb_practiceApp/controller/UserListController.java
+++ src/main/java/in/sakamoto/sb_practiceApp/controller/UserListController.java
@@ -1,4 +1,16 @@
 package in.sakamoto.sb_practiceApp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserListController {
+    /* ユーザー一覧画面の表示 */
+    @GetMapping("/list")
+    public String getUserList() {
+        // ユーザー一覧画面を表示
+        return "user/list";
+    }
 }
