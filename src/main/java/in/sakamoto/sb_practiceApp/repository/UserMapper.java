7a6c527bcccb7bfd827c6fab6fb0817197968678
mocchio <skmt.rome@gmail.com>
2023-05-31 12:10:01 +0900

    ユーザーマスタテーブル用のリポジトリ作成

--- src/main/java/in/sakamoto/sb_practiceApp/repository/UserMapper.java
+++ src/main/java/in/sakamoto/sb_practiceApp/repository/UserMapper.java
@@ -1,4 +1,10 @@
 package in.sakamoto.sb_practiceApp.repository;
 
+import in.sakamoto.sb_practiceApp.domain.user.model.MUser;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper {
+    /* ユーザー登録 */
+    public int insertOne(MUser user);
 }
