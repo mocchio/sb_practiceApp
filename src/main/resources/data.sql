b389510ef43ff88660a66b628f05af7dc12a7a1c
mocchio <skmt.rome@gmail.com>
2023-05-31 10:30:30 +0900

    テーブル追加のsql修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,29 @@
 INSERT INTO employee(id, name, age) VALUES('1', 'Kato', 10)
+
+-- ユーザーマスタ --
+INSERT INTO m_user (
+  user_id
+ ,password
+ ,user_name
+ ,birthday
+ ,age
+ ,gender
+ ,department_id
+ ,role
+) VALUES ('system@co.jp', 'password', 'システム管理者', '2000-01-01', 21, 1, 1, 'ROLE_ADMIN'
+),('user@co.jp', 'password', 'ユーザー1', '2001-02-02', 21, 2, 2, 'ROLE_GENERAL');
+
+-- 部署マスタ --
+INSERT INTO m_department (
+  department_id
+ ,department_name
+) VALUES (1, 'システム管理部'),(2, '営業部');
+
+-- 給料テーブル --
+INSERT INTO t_salary (
+  user_id
+ ,year_month
+ ,salary
+) VALUES ('user@co.jp', '2022/05', 300000)
+,('user@co.jp', '2022/05', 500000)
+,('user@co.jp', '2022/05', 400000);
