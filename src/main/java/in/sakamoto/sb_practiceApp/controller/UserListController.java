18ecdcd25d08ddee7a53957401b9f403884982d4
mocchio <skmt.rome@gmail.com>
2023-06-04 13:14:30 +0900

    コントローラーへユーザー情報取得について記述

--- src/main/java/in/sakamoto/sb_practiceApp/controller/UserListController.java
+++ src/main/java/in/sakamoto/sb_practiceApp/controller/UserListController.java
@@ -1,15 +1,32 @@
 package in.sakamoto.sb_practiceApp.controller;
 
+import in.sakamoto.sb_practiceApp.domain.user.model.MUser;
+import in.sakamoto.sb_practiceApp.domain.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserListController {
+
+    @Autowired
+    private UserService userService;
+
     /* ユーザー一覧画面の表示 */
     @GetMapping("/list")
-    public String getUserList() {
+    public String getUserList(Model model) {
+
+        // ユーザー一覧取得
+        List<MUser> userList = userService.getUsers();
+
+        // Modelに登録
+        model.addAttribute("userList", userList);
+
         // ユーザー一覧画面を表示
         return "user/list";
     }
