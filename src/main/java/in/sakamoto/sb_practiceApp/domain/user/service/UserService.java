8cb3fad4340cc5a6b311614a7c1c48c98749df32
mocchio <skmt.rome@gmail.com>
2023-06-04 13:07:47 +0900

    サービスインターフェースへユーザー情報取得メソッドを追加

--- src/main/java/in/sakamoto/sb_practiceApp/domain/user/service/UserService.java
+++ src/main/java/in/sakamoto/sb_practiceApp/domain/user/service/UserService.java
@@ -2,8 +2,13 @@ package in.sakamoto.sb_practiceApp.domain.user.service;
 
 import in.sakamoto.sb_practiceApp.domain.user.model.MUser;
 
+import java.util.List;
+
 public interface UserService {
 
     /* ユーザー登録 */
     public void signup(MUser user);
+
+    /* ユーザー取得 */
+    public List<MUser> getUsers();
 }
