f0d8976bc9f135ec33703f629739227e11b52504
mocchio <skmt.rome@gmail.com>
2023-06-04 13:09:25 +0900

    サービス実装クラスへユーザー取得メソッドを追加

--- src/main/java/in/sakamoto/sb_practiceApp/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/in/sakamoto/sb_practiceApp/domain/user/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import in.sakamoto.sb_practiceApp.repository.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -18,5 +20,11 @@ public class UserServiceImpl implements UserService {
         user.setDepartmentId(1); // 部署
         user.setRole("ROLE_GENERAL"); // ロール
         mapper.insertOne(user);
+    }
+
+    /* ユーザー取得 */
+    @Override
+    public List<MUser> getUsers() {
+        return mapper.findMany();
     }
 }
