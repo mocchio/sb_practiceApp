fb3a162d8801e3302533dbc49f9c59df8e11cabd
mocchio <skmt.rome@gmail.com>
2023-06-04 13:01:38 +0900

    マッパーへの複数件取得メソッド追加

--- src/main/java/in/sakamoto/sb_practiceApp/repository/UserMapper.java
+++ src/main/java/in/sakamoto/sb_practiceApp/repository/UserMapper.java
@@ -3,9 +3,14 @@ package in.sakamoto.sb_practiceApp.repository;
 import in.sakamoto.sb_practiceApp.domain.user.model.MUser;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
 
     /* ユーザー登録 */
     public int insertOne(MUser user);
+
+    /* ユーザー取得 */
+    public List<MUser> findMany();
 }
